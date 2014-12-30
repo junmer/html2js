c2d9c89410870e9b5a399e9fcdefbdb93d2522bb
menglingjun <menglingjun@baidu.com>
2014-12-30 22:25:00 +0800

    ignore

--- test/test.html.js
+++ test/test.html.js
@@ -1,4 +1,33 @@
-// format
+/**
+ * html2js(source, {
+ *     mode: 'default'
+ *     wrap: 'amd'
+ * })
+ */
+define(function () {return '<div id="i-am-a-id">\n  <div class="i-am-a-class">\n    i am test\n  </div>\n</div>';});
+
+/**
+ * html2js(source, {
+ *     mode: 'default'
+ *     wrap: 'commonjs'
+ * })
+ */
+module.exports = '<div id="i-am-a-id">\n  <div class="i-am-a-class">\n    i am test\n  </div>\n</div>';
+
+/**
+ * html2js(source, {
+ *     mode: 'default'
+ *     wrap: false
+ * })
+ */
+'<div id="i-am-a-id">\n  <div class="i-am-a-class">\n    i am test\n  </div>\n</div>'
+
+/**
+ * html2js(source, {
+ *     mode: 'format'
+ *     wrap: 'amd'
+ * })
+ */
 define(function () {
     return ''
         + '<div id="i-am-a-id">'
@@ -7,7 +36,53 @@ define(function () {
         +   '</div>'
         + '</div>';
 });
-// default
-define(function () {return '<div id="i-am-a-id">\n  <div class="i-am-a-class">\n    i am test\n  </div>\n</div>';});
-// compress
+
+/**
+ * html2js(source, {
+ *     mode: 'format'
+ *     wrap: 'commonjs'
+ * })
+ */
+module.exports = ''
+        + '<div id="i-am-a-id">'
+        +   '<div class="i-am-a-class">'
+        +     'i am test'
+        +   '</div>'
+        + '</div>';
+
+/**
+ * html2js(source, {
+ *     mode: 'format'
+ *     wrap: false
+ * })
+ */
+''
+        + '<div id="i-am-a-id">'
+        +   '<div class="i-am-a-class">'
+        +     'i am test'
+        +   '</div>'
+        + '</div>'
+
+/**
+ * html2js(source, {
+ *     mode: 'compress'
+ *     wrap: 'amd'
+ * })
+ */
 define(function () {return '<div id="i-am-a-id"><div class="i-am-a-class">i am test</div></div>';});
+
+/**
+ * html2js(source, {
+ *     mode: 'compress'
+ *     wrap: 'commonjs'
+ * })
+ */
+module.exports = '<div id="i-am-a-id"><div class="i-am-a-class">i am test</div></div>';
+
+/**
+ * html2js(source, {
+ *     mode: 'compress'
+ *     wrap: false
+ * })
+ */
+'<div id="i-am-a-id"><div class="i-am-a-class">i am test</div></div>'
--- test/test.js
+++ test/test.js
@@ -1,29 +1,91 @@
 /**
- * @file simple test
+ * @file tdd
  */
+
+var vows        = require('vows');
+var assert      = require('assert');
+var extend      = require('extend');
 
 var fs          = require('fs');
 var path        = require('path');
 var html2js     = require('../html2js');
 
-var filename    = 'test2.html';
+var filename    = 'test.html';
 var filePath    = path.resolve( __dirname, filename );
-var html        = fs.readFileSync( filePath , 'utf8' );
-var modes       = ['format', 'default', 'compress'];
+var source      = fs.readFileSync( filePath , 'utf8' );
 
-var output      = modes.map(
-    function (mode) {
-        return ''
-            + '// '
-            + mode
-            + '\n'
-            + html2js(html, {
-                mode: mode,
-                wrap: true
-            });
+// basic case
+var basicCase = {
+
+    'type is string': function(topic) {
+        assert.equal(typeof(topic), 'string');
     }
-).join('\n');
 
-console.log(output);
+};
 
-fs.writeFile( filePath + '.js', output );
+// eval case
+var evalCase = {
+
+    'can be eval': function(topic) {
+        assert.doesNotThrow(function() {
+            eval(topic);
+        }, Error);
+    },
+
+    'eval return type is string': function(topic) {
+        assert.equal(typeof(eval(topic)), 'string');
+    }
+
+};
+
+// Create a Test Suite
+vows.describe('html2js').addBatch({
+
+    'default': extend({
+        topic: function() {
+            return html2js(source);
+        }
+    }, basicCase, evalCase),
+
+    'use format mode': extend({
+        topic: function() {
+            return html2js(source, {mode: 'format'});
+        },
+
+        'has `\\n`': function(topic) {
+            assert.ok(/\n/.test(topic));
+        }
+    }, basicCase, evalCase),
+
+    'use compress mode': extend({
+        topic: function() {
+            return html2js(source, {mode: 'compress'});
+        },
+
+        'has not `\\n`': function(topic) {
+            assert.ok(!/\n/.test(topic));
+        }
+    }, basicCase, evalCase),
+
+    'with amd wrapper': extend({
+        topic: function() {
+            return html2js(source, {wrap: 'amd'});
+        },
+
+        'has `define`': function(topic) {
+            assert.ok(/define/.test(topic));
+        }
+    }, basicCase),
+
+    'with commonjs wrapper': extend(basicCase, {
+        topic: function() {
+            return html2js(source, {wrap: 'commonjs'});
+        },
+
+        'has `module.exports`': function(topic) {
+            assert.ok(/module\.exports/.test(topic));
+        }
+    }, basicCase)
+
+
+}).run(); // Run it
